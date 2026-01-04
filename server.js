c19d6b62a4928a795b431624237cbc1a9ac0fbb7
Vismitha1234 <vvismitha2005@gmail.com>
2026-01-04 13:39:55 +0530

    Update server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// connect MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/libraryDB")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const bookRoutes = require("./routes/books");
+app.use("/books", bookRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
